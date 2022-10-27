5b7d1235ef4e8476025a92e28287e3d40d3fb1d0
DevMazed04 <mdmazedislam2684@gmail.com>
2022-10-27 23:52:26 +0600

    responsive done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import headerImg from '../../images/header.jpg';
 
 const Home = () => {
    return (
-      <div>
-         <h3> Home page</h3>
-      </div>
+      <section>
+         <div className='mt-5 border-danger header mx-auto rounded-4 px-0 py-2  d-flex justify-content-between  align-items-center ps-4 py-3 py-sm-0 header-bg'>
+            <div className='text-start w-50'>
+               <h2>What to learn next?</h2>
+               <small className='text-secondary'>Welcome to my <strong>Programming Path</strong> website. You can visit my website and chosse your favourite programming course which you like. Happy programming</small>
+            </div>
+            <div>
+               <img className='rounded-4' src={headerImg} alt="" width="130px" />
+            </div>
+         </div>
+      </section>
    );
 };
 
