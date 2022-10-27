af55235818d223eeb4dd264b3209f37d8d28eff9
DevMazed04 <mdmazedislam2684@gmail.com>
2022-10-27 09:07:09 +0600

    footer added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 const Footer = () => {
    return (
       <div className='footer p-2 fixed-bottom text-white text-center'>
-         {/* <h5 class="mb-1">Programming Path</h5> */}
+         <h5 class="mb-1">Programming Path</h5>
          <small>Copyright © 2022 - All Rights Reserved.</small>
       </div>
    );
